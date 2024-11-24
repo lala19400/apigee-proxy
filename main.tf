d06f00fa9ab6732d646f8e0f7bd3658361661e4e
LALA <k.hossam19400@outlook.com>
2024-11-24 18:54:30 +0200

    create apigee__proxy

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 # Reference an existing Apigee Organization (created manually in the Console)
 resource "apigee_proxy" "MyProxy" {
   name        = "httpbin"
-  bundle      = "C:\\Users\\pc\\Downloads\\httpbin_rev1_2020_02_02.zip"
-  bundle_hash = "C:\\Users\\pc\\Downloads\\httpbin_rev1_2020_02_02.zip"
-
+  bundle      = "./httpbin_rev1_2020_02_02.zip"
+  bundle_hash = filebase64sha256("./httpbin_rev1_2020_02_02.zip")
 }
 
 # Reference an existing Apigee Environment (created manually in the Console)
