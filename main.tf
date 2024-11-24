0bc151f2e43c63a12d35bf0a0de37a4bbbc5b636
LALA <k.hossam19400@outlook.com>
2024-11-24 02:30:13 +0200

    create apigee__proxy

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Reference an existing Apigee Organization (created manually in the Console)
 resource "apigee_proxy" "MyProxy" {
   name        = "lala-proxy"
-  bundle      = "proxies/MyProxy/MyProxy.zip"
-  bundle_hash = filebase64sha256("proxies/MyProxy/MyProxy.zip")
+  bundle      = "./test-proxy.zip"
+  bundle_hash = filebase64sha256("./test-proxy.zip")
 }
 
 # Reference an existing Apigee Environment (created manually in the Console)
