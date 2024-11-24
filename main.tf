3a47a3ad9f1ea1cd792a7d87b70a4fa56f2f9203
LALA <k.hossam19400@outlook.com>
2024-11-24 18:44:24 +0200

    create apigee__proxy

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 # Reference an existing Apigee Organization (created manually in the Console)
 resource "apigee_proxy" "MyProxy" {
-  name        = "lala-proxy"
-  bundle = "/full/path/to/httpbin_rev1_2020_02_02.zip"
-bundle_hash = filebase64sha256("/full/path/to/httpbin_rev1_2020_02_02.zip")
+  name        = "httpbin"
+  bundle      = "/full/path/to/httpbin_rev1_2020_02_02.zip"
+  bundle_hash = filebase64sha256("/full/path/to/httpbin_rev1_2020_02_02.zip")
 
 }
 
 # Reference an existing Apigee Environment (created manually in the Console)
 resource "apigee_proxy_deployment" "example" {
-  proxy_name       = "lala-proxy"
+  proxy_name       = "httpbin"
   environment_name = "test-env" # Replace with your environment name (e.g., dev, prod)
   revision         = 1
 }
