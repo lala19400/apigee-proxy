49cba0d7e4f77c053b6e4a128e2967c9298bd74d
LALA <k.hossam19400@outlook.com>
2024-11-25 00:43:42 +0200

    create apigee__proxy

--- main.tf
+++ main.tf
@@ -10,4 +10,10 @@ resource "apigee_proxy_deployment" "example" {
   proxy_name       = "helloworld"
   environment_name = "test-env" # Replace with your environment name (e.g., dev, prod)
   revision         = "1"
+  depends_on       = [apigee_proxy.MyProxy]
+
+  # Add delay before the deployment
+  provisioner "local-exec" {
+    command = "sleep 15" # Adjust the delay duration as needed
+  }
 }
