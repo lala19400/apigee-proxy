f2a4d5db6a2325040709e6d6977cbd9c7803d78f
LALA <k.hossam19400@outlook.com>
2024-11-25 00:58:29 +0200

    create apigee__proxy

--- providers.tf
+++ providers.tf
@@ -10,8 +10,5 @@ provider "google-beta" {
 
 provider "apigee" {
   organization = "academic-matter-437700-t0"
-  #server       = "apigee.googleapis.com"
-  #server_path  = "v1"
-  #use_ssl      = true
-  #port         = 443
+  server       = "apigee.googleapis.com"
 }
