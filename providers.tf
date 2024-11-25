451153747ba1f9be6626e07dd3ff30d71739fdb4
LALA <k.hossam19400@outlook.com>
2024-11-25 22:19:13 +0200

    create apigee__proxy

--- providers.tf
+++ providers.tf
@@ -1,11 +1,14 @@
 provider "google" {
-  project = "academic-matter-437700-t0"
-  region  = "us-central1"
+  project                = "academic-matter-437700-t0"
+  region                 = "us-central1"
+  apigee_custom_endpoint = "https://sa-apigee-googleapis.com/v1/"
+
 }
 
 provider "google-beta" {
-  project = "academic-matter-437700-t0"
-  region  = "us-central1"
+  project                = "academic-matter-437700-t0"
+  region                 = "us-central1"
+  apigee_custom_endpoint = "https://sa-apigee-googleapis.com/beta/v1"
 }
 
 provider "apigee" {
