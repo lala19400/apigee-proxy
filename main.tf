c615609bf0b6b01c4ee41c4a77d90644ef23c364
LALA <k.hossam19400@outlook.com>
2024-11-25 22:00:08 +0200

    create apigee__proxy

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "apigee_proxy_deployment" "example" {
   proxy_name       = "helloworld"
   environment_name = "test-env" # Replace with your environment name (e.g., dev, prod)
   revision         = "1"
-  serviceaccount   = "apigee-proxy@academic-matter-437700-t0.iam.gserviceaccount.com"
+  service_account   = "apigee-proxy@academic-matter-437700-t0.iam.gserviceaccount.com"
   depends_on       = [apigee_proxy.MyProxy]
 
   # # Add delay before the deployment
